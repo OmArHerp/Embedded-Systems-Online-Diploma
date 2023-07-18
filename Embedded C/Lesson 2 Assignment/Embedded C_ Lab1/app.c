20e8299619b7f8f9d4723d0a1f3ce9db9cea7259
OmArHerp <112272265+OmArHerp@users.noreply.github.com>
2023-07-18 21:47:30 +0300

    Lab1 of Lesson 2 in Embedded C

---
+++ Embedded C/Lesson 2 Assignment/Embedded C_ Lab1/app.c
@@ -0,0 +1,8 @@
+#include "uart.h"
+unsigned char string_buffer[100] = "learn-in-depth:HERP" ;
+unsigned char const string_buffer2[100] = "learn-in-depth:HERP" ;
+
+void main(void)
+{
+	Uart_Send_string (string_buffer);
+}
